90ff0b7389b6af6a6241028403f6031de56acce7
Alexey Gorkov <gorkoff@gmail.com>
2019-03-31 20:36:06 +0300

    Пустой проект

---
+++ src/PBoxSolver.dpr
@@ -0,0 +1,14 @@
+program PBoxSolver;
+
+uses
+  Vcl.Forms,
+  UformMain in 'UformMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
