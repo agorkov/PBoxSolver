6cd4cd69655a600f8c109d93da91cda4b655f17c
Alexey Gorkov <gorkoff@gmail.com>
2019-03-31 21:54:12 +0300

    Расчет карты и вывод

--- src/PBoxSolver.dpr
+++ src/PBoxSolver.dpr
@@ -2,8 +2,13 @@ program PBoxSolver;
 
 uses
   Vcl.Forms,
-  UformMain in 'UformMain.pas' {Form1} ,
-  UGlobalTypes in 'UGlobalTypes.pas';
+  UformMain in 'UformMain.pas' {Form1},
+  UGlobalTypes in 'UGlobalTypes.pas',
+  UBinaryImages in '..\..\ImgSharedUnits\src\UBinaryImages.pas',
+  UBitMapFunctions in '..\..\ImgSharedUnits\src\UBitMapFunctions.pas',
+  UColorImages in '..\..\ImgSharedUnits\src\UColorImages.pas',
+  UGrayscaleImages in '..\..\ImgSharedUnits\src\UGrayscaleImages.pas',
+  UPixelConvert in '..\..\ImgSharedUnits\src\UPixelConvert.pas';
 
 {$R *.res}
 
